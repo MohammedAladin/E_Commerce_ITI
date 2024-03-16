f1aeddcb45f4083f6e3d9b4df5a5cbc420d8cadf
hassan kamal <hassankamal87992010@gmail.com>
2024-03-16 16:38:59 +0200

    handle conflict

--- src/main/webapp/app/assets/js/SignInAjax.js
+++ src/main/webapp/app/assets/js/SignInAjax.js
@@ -3,43 +3,41 @@ document.getElementById('registrationForm').addEventListener('submit', function(
 
     event.preventDefault();
 
-    window.location.href = "cart.jsp";
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password-in').value;
+    var email = document.getElementById('Email-in').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+    var phoneNumber = document.getElementById('phoneNumber').value;
+    var dob = document.getElementById('dob').value;
 
-//    var username = document.getElementById('username').value;
-//    var password = document.getElementById('password-in').value;
-//    var email = document.getElementById('Email-in').value;
-//    var confirmPassword = document.getElementById('confirmPassword').value;
-//    var phoneNumber = document.getElementById('phoneNumber').value;
-//    var dob = document.getElementById('dob').value;
-//
-//    console.log("username: " + username);
-//
-//
-//    var signUpData = {
-//        username: username,
-//        password: password,
-//        email: email,
-//        confirmPassword: confirmPassword,
-//        phoneNumber: phoneNumber,
-//        dob: dob
-//    };
-//
-//    let xhr = new XMLHttpRequest();
-//
-//    xhr.open('POST', 'app/Register', true);
-//    xhr.setRequestHeader('Content-Type', 'application/json');
-//
-//    xhr.onreadystatechange = function() {
-//        console.log("readyState: " + xhr.readyState);
-//        if (xhr.readyState === 4 && xhr.status === 200) {
-//            console.log("success");
-//            console.log(xhr.responseText);
-//            window.location.href = "app/Home";
-//        }
-//    }
-//
-//    xhr.send(JSON.stringify(signUpData));
-//
-//
-//
+    console.log("username: " + username);
+
+
+    var signUpData = {
+        username: username,
+        password: password,
+        email: email,
+        confirmPassword: confirmPassword,
+        phoneNumber: phoneNumber,
+        dob: dob
+    };
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('POST', 'app/Register', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+
+    xhr.onreadystatechange = function() {
+        console.log("readyState: " + xhr.readyState);
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log("success");
+            console.log(xhr.responseText);
+            window.location.href = "app/Home";
+        }
+    }
+
+    xhr.send(JSON.stringify(signUpData));
+
+
+
 });
